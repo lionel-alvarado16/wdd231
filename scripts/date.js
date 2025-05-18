2a4bf47c7fba59e81364026ffd9afeee08921cb5
lionel-alvarado16 <alv23013@byui.edu>
2025-05-18 03:28:50 -0500

    Home Page Complete

---
+++ scripts/date.js
@@ -0,0 +1,15 @@
+const currentyear = document.querySelector('#currentyear');
+const lastModified = document.querySelector('#lastModified');
+
+const today = new Date();
+
+function updateFooterContent() {
+    if (window.matchMedia("(min-width: 37.5em)").matches) {
+        currentyear.innerHTML = `&copy;${today.getFullYear()} 🎸 Lionel Alexander Alvarado 🎸 Peru `;
+    } else {
+        currentyear.innerHTML = `&copy;${today.getFullYear()}<br><span>🎸 Lionel Alexander Alvarado 🎸</span><br><span>Peru</span>`;
+    }
+}
+
+updateFooterContent();
+lastModified.innerHTML = `Last Update: ${document.lastModified}`;
