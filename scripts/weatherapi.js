6807f2ed3cca1b0a9315152d39aab7b681c9df39
lionel-alvarado16 <alv23013@byui.edu>
2025-07-24 12:31:33 -0500

    Consuming an API Activity Complete

---
+++ scripts/weatherapi.js
@@ -0,0 +1,39 @@
+// Create HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// Create required values for the URL
+const myKey = "2c6a2285ce9048f33ec9132f22845bda";
+const myLat = "49.75";
+const myLong = "6.64";
+
+// Consutruct a full path using template literals
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&units=metric&appid=${myKey}`;
+
+// Try to grab the current weather data
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            displayResults(data); // uncommnet when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
