f6d37a321dde2d0fbdc16318e49760c3d87a8cc0
lionel-alvarado16 <alv23013@byui.edu>
2025-08-11 13:57:25 -0500

    Discover Page Progress

--- chamber/scripts/cc-discover.js
+++ chamber/scripts/cc-discover.js
@@ -20,12 +20,13 @@ async function getItemData() {
 const displayItems = (items) => {
     items.forEach((item) => {
         // Create elemets to add to the div cards element
-        let card = document.createElement('section');
+        let card = document.createElement('div');
         let title = document.createElement('h2');
         let image = document.createElement('figure');
         let picture = document.createElement('img');
         let address = document.createElement('address');
         let description = document.createElement('p');
+        let button = document.createElement('button');
 
         title.innerHTML = `${item.name}`;
 
@@ -36,13 +37,15 @@ const displayItems = (items) => {
         picture.setAttribute('height', '200');
         image.appendChild(picture);
 
-        address.innerHTML = `${item.address}`;
+        address.innerHTML = `Location: ${item.address}`;
         description.innerHTML = `${item.description}`;
+        button.innerHTML = "Learn More";
 
         card.appendChild(title);
-        card.appendChild(picture);
+        card.appendChild(image);
         card.appendChild(address);
         card.appendChild(description);
+        card.appendChild(button);
 
         cards.appendChild(card);
     });
