c3b35d3d0bd0eb1241a3c4cf6b0f8173bc21986d
lionel-alvarado16 <alv23013@byui.edu>
2025-08-13 03:28:23 -0500

    Final Project Progress

---
+++ finalproject/scripts/thanks.js
@@ -0,0 +1,13 @@
+const myInfo = new URLSearchParams(window.location.search);
+console.log(myInfo);
+
+document.querySelector('#results').innerHTML = `
+<h2>Membership Details</h2>
+<p><strong>First Name:</strong> ${myInfo.get('fname')}</p>
+<p><strong>Last Name:</strong> ${myInfo.get('lname')}</p>
+<p><strong>Email:</strong> ${myInfo.get('email')}</p>
+<p><strong>Birthdate:</strong> ${myInfo.get('birthdate')}</p>
+<p><strong>Cell Phone:</strong> ${myInfo.get('phone')}</p>
+<p><strong>Event's Name:</strong> ${myInfo.get('event')}</p>
+<p><strong>Comments:</strong> ${myInfo.get('comments')}</p>
+`;
